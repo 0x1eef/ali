af0f5bd74457f61c9f59235b5203a0f1b10c508b
0x1eef <0x1eef@hardenedbsd.org>
2026-03-04 22:57:39 -0300

    Add example

---
+++ _examples/Multimodal/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/0x1eef/ali"
+	"github.com/0x1eef/ali/provider"
+)
+
+func main() {
+	p, err := provider.New(ali.OpenAI)
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := p.Complete(
+		ali.WithText("Describe the image"),
+		ali.WithImageUrl("https://upload.wikimedia.org/wikipedia/commons/3/3f/Fronalpstock_big.jpg"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	text, err := c.Text()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", text)
+}
