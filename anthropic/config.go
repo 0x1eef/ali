b87143fc9c168e72a8b8aec18ff30a9f719687fb
0x1eef <0x1eef@hardenedbsd.org>
2026-03-05 02:05:04 -0300

    Remove duplication

--- anthropic/config.go
+++ anthropic/config.go
@@ -1,11 +1,5 @@
 package anthropic
 
-import (
-	"context"
-
-	"github.com/0x1eef/ali"
-)
-
 // WithToken sets the Anthropic API token.
 func WithToken(token string) func(o *Anthropic) {
 	return func(a *Anthropic) {
@@ -18,39 +12,4 @@ func WithHost(host string) func(o *Anthropic) {
 	return func(a *Anthropic) {
 		a.host = host
 	}
-}
-
-// WithText sets the user prompt text.
-func WithText(prompt string) func(r *ali.CompletionConfig) {
-	return ali.WithText(prompt)
-}
-
-// WithImageUrl includes an image URL with a request.
-func WithImageUrl(url string) func(r *ali.CompletionConfig) {
-	return ali.WithImageUrl(url)
-}
-
-// WithRole sets the role for the prompt message.
-func WithRole(role string) func(r *ali.CompletionConfig) {
-	return ali.WithRole(role)
-}
-
-// WithModel sets the model identifier.
-func WithModel(model string) func(r *ali.CompletionConfig) {
-	return ali.WithModel(model)
-}
-
-// WithParams sets request query parameters.
-func WithParams(params ali.Params) func(r *ali.CompletionConfig) {
-	return ali.WithParams(params)
-}
-
-// WithContext sets the request context.
-func WithContext(ctx context.Context) func(r *ali.CompletionConfig) {
-	return ali.WithContext(ctx)
-}
-
-// WithMessages appends prior conversation messages.
-func WithMessages(msgs []ali.Message) func(r *ali.CompletionConfig) {
-	return ali.WithMessages(msgs)
 }
--- openai/config.go
+++ openai/config.go
@@ -1,11 +1,5 @@
 package openai
 
-import (
-	"context"
-
-	"github.com/0x1eef/ali"
-)
-
 // WithToken sets the OpenAI API token.
 func WithToken(token string) func(o *OpenAI) {
 	return func(o *OpenAI) {
@@ -18,38 +12,4 @@ func WithHost(host string) func(o *OpenAI) {
 	return func(o *OpenAI) {
 		o.Host = host
 	}
-}
-
-// WithText sets the user prompt text.
-func WithText(prompt string) func(r *ali.CompletionConfig) {
-	return ali.WithText(prompt)
-}
-
-func WithImageUrl(url string) func(r *ali.CompletionConfig) {
-	return ali.WithImageUrl(url)
-}
-
-// WithRole sets the role for the prompt message.
-func WithRole(role string) func(r *ali.CompletionConfig) {
-	return ali.WithRole(role)
-}
-
-// WithModel sets the model identifier.
-func WithModel(model string) func(r *ali.CompletionConfig) {
-	return ali.WithModel(model)
-}
-
-// WithParams sets request query parameters.
-func WithParams(params ali.Params) func(r *ali.CompletionConfig) {
-	return ali.WithParams(params)
-}
-
-// WithContext sets the request context.
-func WithContext(ctx context.Context) func(r *ali.CompletionConfig) {
-	return ali.WithContext(ctx)
-}
-
-// WithMessages appends prior conversation messages.
-func WithMessages(msgs []ali.Message) func(r *ali.CompletionConfig) {
-	return ali.WithMessages(msgs)
 }
