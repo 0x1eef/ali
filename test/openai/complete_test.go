5def34f1697ef503682b58589394f23af6f85b5e
0x1eef <0x1eef@hardenedbsd.org>
2026-03-09 01:54:16 -0300

    Rewrite test with provider.New

--- test/openai/complete_test.go
+++ test/openai/complete_test.go
@@ -1,26 +1,21 @@
 package openai_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/0x1eef/ali"
-	"github.com/0x1eef/ali/openai"
+	"github.com/0x1eef/ali/provider"
+
 	"github.com/0x1eef/ali/test/assert"
 	"github.com/0x1eef/ali/test/vcr"
 )
 
 func TestComplete(t *testing.T) {
-	token := os.Getenv("OPENAI_SECRET")
-	if token == "" {
-		t.Fatalf("OPENAI_SECRET is required")
-	}
+	p, err := provider.New(ali.OpenAI)
+	assert.AssertNil(t, err)
 
 	client, stop := vcr.NewHTTPClient(t, "complete")
 	defer stop()
-
-	p, err := openai.New(openai.WithToken(token))
-	assert.AssertNil(t, err)
 
 	comp, err := p.Complete(
 		ali.WithText("Reply with this exact string: 'pong'"),
